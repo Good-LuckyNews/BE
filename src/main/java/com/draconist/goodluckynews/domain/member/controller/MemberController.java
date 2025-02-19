d1803f41e6d8c0416016c1719df839e0e6fb28ae
oculo0204 <oculo0204@gmail.com>
2025-02-20 03:47:23 +0900

    #3 feat: login initial feature

---
+++ src/main/java/com/draconist/goodluckynews/domain/member/controller/MemberController.java
@@ -0,0 +1,35 @@
+package com.draconist.goodluckynews.domain.member.controller;
+
+import com.draconist.goodluckynews.domain.member.dto.JoinDTO;
+import com.draconist.goodluckynews.domain.member.dto.LoginRequestDTO;
+import com.draconist.goodluckynews.domain.member.service.MemberService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/member")
+@RequiredArgsConstructor
+public class MemberController {
+    private final MemberService memberService;
+
+    /**
+     * 회원 로그인
+     */
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@Valid @RequestBody LoginRequestDTO loginRequestDTO) {
+        return memberService.login(loginRequestDTO);
+    }
+
+    /**
+     * 회원 가입
+     */
+    @PostMapping("/join")
+    public ResponseEntity<?> join(@Valid @RequestBody JoinDTO joinDTO) {
+        return memberService.join(joinDTO);
+    }
+}
