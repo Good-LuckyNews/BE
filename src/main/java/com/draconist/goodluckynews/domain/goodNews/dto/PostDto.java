c757a150d5c45a97c1080214c67addadce79e3e1
gimchaewon <gimchaewon417@gmail.com>
2025-03-09 15:48:02 +0900

    fix: 좋아요 및 댓글 수 포함

--- src/main/java/com/draconist/goodluckynews/domain/goodNews/dto/PostDto.java
+++ src/main/java/com/draconist/goodluckynews/domain/goodNews/dto/PostDto.java
@@ -10,12 +10,14 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @Builder
 public class PostDto {
-    private Long postId;       // 게시글 ID (조회 시 사용)
-    private String title;      // 게시글 제목 (검색 및 조회 시 사용)
+    private Long postId;       // 게시글 ID
+    private String title;      // 게시글 제목
     private Long placeId;      // 게시글이 속한 장소 ID
     private Long userId;       // 게시글 작성자 ID
     private String content;    // 게시글 내용
     private String image;      // 이미지 URL (선택 사항)
     private LocalDateTime createdAt; // 생성 날짜
     private LocalDateTime updatedAt; // 수정 날짜
+    private int likeCount;     // 좋아요 개수 추가
+    private int commentCount;  // 댓글 개수 추가
 }
