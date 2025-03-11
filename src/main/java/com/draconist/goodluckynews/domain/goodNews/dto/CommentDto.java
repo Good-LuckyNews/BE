c48010c76926d182607092b261818564221d6c7d
oculo0204 <oculo0204@gmail.com>
2025-03-11 22:55:55 +0900

    fix: title, 댁슬 userId 제외

--- src/main/java/com/draconist/goodluckynews/domain/goodNews/dto/CommentDto.java
+++ src/main/java/com/draconist/goodluckynews/domain/goodNews/dto/CommentDto.java
@@ -13,7 +13,6 @@ import java.util.List;
 public class CommentDto {
     private Long commentId;  // 댓글 ID
     private Long postId;     // 댓글이 속한 게시글 ID
-    private Long userId;     // 댓글 작성자 ID
     private String content;  // 댓글 내용
     private LocalDateTime createdAt; // 작성 날짜
     private int likeCount;   // 댓글 좋아요 개수 추가
