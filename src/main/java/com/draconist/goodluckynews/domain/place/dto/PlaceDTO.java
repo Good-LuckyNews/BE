ebbdc6551a6ac021bdc2f811eb3336f30caa4e74
gimchaewon <gimchaewon417@gmail.com>
2025-03-12 16:08:43 +0900

    fix: 플레이스 북마크 수정

--- src/main/java/com/draconist/goodluckynews/domain/place/dto/PlaceDTO.java
+++ src/main/java/com/draconist/goodluckynews/domain/place/dto/PlaceDTO.java
@@ -13,4 +13,6 @@ public class PlaceDTO {
     private String placeImg;
     private int likeCount; // 좋아요 수 추가
     private boolean isLiked; // 좋아요 여부
+    private boolean isBookmark; // 북마크 여부 추가 (이전 isLiked → isBookmark)
+
 }
